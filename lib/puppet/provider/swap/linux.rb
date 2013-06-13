692313c69eb6238d42c6e82734e5c3355fa7abad
Steve Huff <shuff@vecna.org>
2013-06-13 13:36:23 -0400

    The minimum necessary provider

--- lib/puppet/provider/swap/linux.rb
+++ lib/puppet/provider/swap/linux.rb
@@ -8,11 +8,41 @@ Puppet::Type.type(:swap).provide :linux do
     commands :swapoff => '/sbin/swapoff'
     commands :swaplist => '/sbin/swapon -s'
 
+    def create
+        begin
+            mkswap(@resource[:path])
+        rescue Puppet::ExecutionFailure
+            raise Puppet::ParseError("Unable to initialize swap on '#{@resource[:path]}'.")
+        end
+        begin
+            swapon(@resource[:path])
+        rescue Puppet::ExecutionFailure
+            raise Puppet::ParseError("Unable to enable swap on '#{@resource[:path]}'.")
+        end
+    end
+
+    def exists?
+        swaps.include?(@resource[:path])
+    end
+
+    def destroy
+        begin
+            swapoff(@resource[:path])
+        rescue Puppet::ExecutionFailure
+            raise Puppet::ParseError("Unable to disable swap on '#{@resource[:path]}'.")
+        end
+    end
+
     def swaps
-        raw = Array(swaplist).split(/\n/)
+        raw = Array.new()
+        cooked = Array.new()
+        begin
+            raw = Array(swaplist).split(/\n/)
+        rescue Puppet::ExecutionFailure
+            nil
+        end
         # the first line is the header
         raw.shift
-        cooked = Array()
         raw.each do |line|
             path = String(line).split(' ', 2).shift
             cooked.push(path)
