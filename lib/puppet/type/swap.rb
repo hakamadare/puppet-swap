3a0a65ec1656b79241f071dc518fa81a92d7a089
Steve Huff <shuff@vecna.org>
2013-06-13 13:48:49 -0400

    Some parameter validation

--- lib/puppet/type/swap.rb
+++ lib/puppet/type/swap.rb
@@ -1,8 +1,19 @@
 Puppet::Type.newtype(:swap) do
+    require 'pathname'
     ensurable
 
     newparam(:path) do
         desc "The path to the swap volume or file."
         isnamevar
+
+        validate do |value|
+            p = Pathname.new(value)
+            if ! p.absolute?
+                raise Puppet::ParseError("'#{value}' is not an absolute path.")
+            end
+            if ! p.exists?
+                raise Puppet::ParseError("'#{value}' must be created first.")
+            end
+        end
     end
 end
